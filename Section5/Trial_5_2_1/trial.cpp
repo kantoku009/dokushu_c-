2a9c04ec5747e7120115f18a696492d3d8c72884
kantoku009 <kantoku009@hotmail.com>
2016-02-11 11:31:53 +0900

    第5章

--- Section5/Trial_5_2_1/trial.cpp
+++ Section5/Trial_5_2_1/trial.cpp
@@ -1,6 +1,11 @@
 /**
  * @file	trial.cpp
  * @brief	独習C++ 練習問題 5.2 1
+ * @note	関数からの戻り値がインスタンスの場合,
+ *			コンパイラーによってはコピーコンストラクターが起動されない.
+ *			■参考
+ *			　コピーの省略 (Wikipedia)
+ *			　https://ja.wikipedia.org/wiki/%E3%82%B3%E3%83%94%E3%83%BC%E3%81%AE%E7%9C%81%E7%95%A5
  */
 #include <iostream>
 using namespace std;
